77dbe0e27d16f7ac9e874a35e452da418b4e846d
Philipp Kats <casyfill@gmail.com>
2016-04-04 13:29:54 -0400

    setup.py version

--- setup.py
+++ setup.py
@@ -3,74 +3,14 @@
 Version handling borrowed from geopandas project.
 """
 
-import sys
-import os
-import warnings
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-LONG_DESCRIPTION = """Simple notifier to decorate your long functions
-"""
-
-MAJOR = 0
-MINOR = 1
-MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-QUALIFIER = ''
-if os.environ.get('READTHEDOCS', False) == 'True':
-    INSTALL_REQUIRES = []
-else:
-    INSTALL_REQUIRES = ['pync']
-
-FULLVERSION = VERSION
-if not ISRELEASED:
-    FULLVERSION += '.dev'
-    try:
-        import subprocess
-        try:
-            pipe = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
-                                    stdout=subprocess.PIPE).stdout
-        except OSError:
-            # msysgit compatibility
-            pipe = subprocess.Popen(
-                ["git.cmd", "describe", "HEAD"],
-                stdout=subprocess.PIPE).stdout
-        rev = pipe.read().strip()
-        # makes distutils blow up on Python 2.7
-        if sys.version_info[0] >= 3:
-            rev = rev.decode('ascii')
-
-        FULLVERSION = '%d.%d.%d.dev-%s' % (MAJOR, MINOR, MICRO, rev)
-
-    except:
-        warnings.warn("WARNING: Couldn't get git revision")
-else:
-    FULLVERSION += QUALIFIER
-
-
-def write_version_py(filename=None):
-    cnt = """\
-version = '%s'
-short_version = '%s'
-"""
-    if not filename:
-        filename = os.path.join(
-            os.path.dirname(__file__), 'inotifyer', 'version.py')
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % (FULLVERSION, VERSION))
-    finally:
-        a.close()
-
-write_version_py()
+from setuptools import setup
+LONG_DESCRIPTION = ''' Ipython notifyer is dependent on pync package and
+meant as a tiny decorator for your heavy long functions. It is especially
+useful in the Ipython, as web browser can lead to the massive destruction :-)
+'''
 
 setup(name='inotifyer',
-      version=FULLVERSION,
+      version='0.1.a',
       description='simple notification decorator',
       license='BSD',
       author='Philipp Kats',
@@ -78,4 +18,4 @@ setup(name='inotifyer',
       url='https://github.com/Casyfill/ipython_notifier',
       long_description=LONG_DESCRIPTION,
       packages=['ipynotifyer'],
-      install_requires=INSTALL_REQUIRES)
+      install_requires=('pync'))
