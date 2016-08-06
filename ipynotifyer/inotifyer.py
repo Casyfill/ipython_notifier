e13ffbaadeca2ad95e1d558e89dc71758a7a5ae6
Philipp Kats <casyfill@gmail.com>
2016-08-06 15:49:01 -0400

    timer added

--- ipynotifyer/inotifyer.py
+++ ipynotifyer/inotifyer.py
@@ -1,5 +1,5 @@
 from pync import Notifier
-
+import time
 
 def notifyMe(txt, act='com.google.Chrome', title='Jupyter Nb'):
     '''
@@ -8,16 +8,25 @@ def notifyMe(txt, act='com.google.Chrome', title='Jupyter Nb'):
     Notifier.notify(txt, activate=act, title=title)
 
 
-def notifyOnComplete(func):
-    '''
-    function used to decorate another
+def notifyOnComplete(func, timer=False):
+    '''function used to decorate another
     long function in python notebook:
     prints a notifier on complete
+
+    Args:
+        func(obj): function to run
+        timer(bool): if true, will print time spent on function
     '''
-    def notifyOnFinish(*args, **kwargs):
+    def notifyOnFinish(timer=timer):
+        if timer:
+            start = time.now()
         try:
             r = func(*args, **kwargs)
-            notifyMe('Job complete!')
+            if timer:
+                delta = time.now() - start
+                notifyMe('Job complete in {0} seconds'.format(delta.total_seconds()))
+            else:
+                notifyMe('Job complete!')
             return r
         except Exception, e:
             notifyMe('Exception raised:{}'.format(e))
