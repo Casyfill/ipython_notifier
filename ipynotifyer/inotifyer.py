c52cc9ec8e7a75550d8d86bbaaeb8bf731912f60
Philipp Kats <casyfill@gmail.com>
2016-08-06 16:30:18 -0400

    timing again

--- ipynotifyer/inotifyer.py
+++ ipynotifyer/inotifyer.py
@@ -1,28 +1,31 @@
 from pync import Notifier
-import time
+from time import time
 
 
-def notifyMe(txt, act='com.google.Chrome', title='Jupyter Nb'):
+def _notifyMe(txt, act='com.google.Chrome', title='Jupyter Nb'):
     '''
     rises notification with text passed, opens safary on click
     '''
     Notifier.notify(txt, activate=act, title=title)
 
 
-def notifyOnComplete(timer=False):
+def notifyOnComplete(timer=None):
     def decorator(f):
+        f.timer = timer
+
         def wrapper(*args, **kwargs):
+            timer = f.timer
             if timer:
                 start = time.now()
             try:
                 r = f(*args, **kwargs)
                 if timer:
                     delta = (time.now() - start).total_seconds()
-                    notifyMe('Job complete in {0} seconds'.format(delta))
+                    _notifyMe('Job complete in {0} seconds'.format(delta))
                 else:
-                    notifyMe('Job complete!')
+                    _notifyMe('Job complete!')
                 return r
             except Exception, e:
-                notifyMe('Exception raised:{}'.format(e))
+                _notifyMe('Exception raised:{}'.format(e))
         return wrapper
     return decorator
